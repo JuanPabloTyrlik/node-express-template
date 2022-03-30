f457aa3ba573fe8fc1ada064852d389d7d2bbd28
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2022-03-29 23:22:08 -0300

    Add .env.example

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -1,10 +1,10 @@
 import cors from 'cors';
-import { Application, json, NextFunction, Request, Response } from 'express';
+import { Application, json, Request, Response } from 'express';
 
 export const setupMiddlewares = (app: Application) => {
   app.use(cors());
   app.use(json());
-  app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
+  app.use((err: Error, _: Request, res: Response) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
